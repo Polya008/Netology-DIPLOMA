1ffffd1d897bd65d7a2e995bc3e0e8de53f0cebc
Polya008 <polskaya007@mail.ru>
2024-02-10 23:40:37 +0300

    Updates

--- src/components/SearchResults/SearchResultstrain.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import TrainCard from "./Train/TrainCard";
-
-export default function SearchResultsTrain({ data }) {
-  console.log("SearchResultsTrain", data);
-  return <div>recm</div>;
-}

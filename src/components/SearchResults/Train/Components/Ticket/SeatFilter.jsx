63832acd35c24eed4ece6702bc5be21e21463882
Polya008 <polskaya007@mail.ru>
2024-02-10 22:51:46 +0300

    version 3.1

--- src/components/SearchResults/Train/Components/Ticket/SeatFilter.jsx
+++ src/components/SearchResults/Train/Components/Ticket/SeatFilter.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { useState } from "react";
+import React from "react";
 
 export default function SeatFilter({ name, setSearchParams, searchParams }) {
   const main = classNames({
--- src/components/SearchResults/Train/Components/TicketHeader.jsx
+++ src/components/SearchResults/Train/Components/TicketHeader.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import MyInput from "../../../UI/MyInput";
 
 export default function TicketHeader({ setSeatCount }) {
   return (
